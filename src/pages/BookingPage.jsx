f66f996ee2cf6476726a5dadc40ef30861668dcb
Anthony <akingori371@gmail.com>
2026-08-30 22:41:19 +0300

    create Firebase.js

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../Firebase";
 
 const DOCTORS = [
   { name: "Dr John Kamau", speciality: "Surgeon" },
